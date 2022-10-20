dcdcf8b53df3ccaa2ce3e25b57f151f38c9a74bc
Henry W <henry.woodmansee@gmail.com>
2022-10-20 13:43:27 -0700

    rever-string done

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,21 @@
 module.exports = function reverseString(string) {
   // Your code here
+  if (typeof string === 'string') {
+    return string.split('').reverse().join('')
+  } else {
+    throw Error()
+  }
 };
+
+// try {
+// reverseString()
+// } catch (error) {
+//   console.log(error.message)
+// }
+// module.exports = function reverseString(string) {
+//   // Your code here
+//   if (typeof string === 'string') {
+//     return string.split('').reverse().join('')
+//   }
+//   throw Error()
+// }
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,32 @@
 // Your code here
+const chai = require('chai')
+const expect = chai.expect
+const reverseString = require('../problems/reverse-string')
+
+let nonString1
+let nonString2
+let nonString3
+describe('Reverse String Function', () => {
+    let word = 'fun'
+    it('should reverse string', () => {
+        // expect((() => reverseString(word))()).to.equal('nuf')
+        expect(reverseString(word)).to.equal('nuf')
+    })
+    nonString1 = true
+    nonString2 = {}
+    nonString3 = 100
+
+    it('should throw an error if argument is not a string', () => {
+        // expect(() => reverseString(nonString1)).to.throw(Error)
+        // expect(() => reverseString(nonString2)).to.throw(Error)
+        // expect(() => reverseString(nonString3)).to.throw(Error)
+        expect(() => reverseString(nonString1)).to.throw(Error)
+        expect(() => reverseString(nonString2)).to.throw(Error)
+        expect(() => reverseString(nonString3)).to.throw(Error)
+    })
+})
+/**
+ * this function
+ *      should do this
+ *      should do that
+ */
