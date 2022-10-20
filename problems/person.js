c2e46c25d09b54c0c04c1cf819f2d893f04c4bee
Sophie Main <sophiekmain@gmail.com>
2022-10-20 18:22:07 -0500

    person prob

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,14 @@
 class Person {
-  // Your code here
+  constructor(firstname, age){
+    this.firstname = firstname
+    this.age = age
+  }
+
+  sayHello(){
+    let greetingMessage = `${this.firstname} says hello`
+    return greetingMessage
+  }
+
 }
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,32 @@
-// Your code here
+const chai = require('chai')
+const spies = require('chai-spies');
+const Person = require('../problems/person');
+chai.use(spies)
+const expect = chai.expect;
+
+describe('Person class', ()=> {
+    let person
+    beforeEach(()=>{
+        person = new Person('mai', 32)
+    })
+
+    describe('Constructor', ()=> {
+        it('should set the name and age properties', ()=>{
+            expect(person).to.have.property('firstname')
+            expect(person).to.have.property('age')
+            expect(person.firstname).to.equal('mai')
+            expect(person.age).to.equal(32)
+        })
+    })
+
+    describe('sayHello', ()=> {
+        it('should return a string of the Person instances name and a greeting message', ()=> {
+            //let greetingMessage = `${Person.firstname} says hello`
+            expect(person.sayHello()).to.equal('mai says hello')
+        })
+
+    })
+    //describe('')
+
+
+})
